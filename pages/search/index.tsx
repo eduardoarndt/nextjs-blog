db8e60ad57c16e32289b58faf49183fb1ec7d93c
Eduardo Arndt <eduardo.arndt@sap.com>
2023-03-31 10:57:40 -0300

    organize imports

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,8 +1,5 @@
-import { GetStaticProps } from "next";
 import Layout from "../../components/layout";
 import SearchBar from "../../components/searchBar";
-import { Country } from "../../domain/country";
-import { getCountry } from "../../lib/countries";
 
 export default function Search() {
   return (
