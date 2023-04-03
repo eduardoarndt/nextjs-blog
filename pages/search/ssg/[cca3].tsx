432363ba455365b6f25b17afdb5b9eeac7fe7cca
Eduardo Arndt <eduardo.arndt@sap.com>
2023-04-03 16:02:18 -0300

    fix placeholder for search

--- pages/search/ssg/[cca3].tsx
+++ pages/search/ssg/[cca3].tsx
@@ -16,14 +16,7 @@ export default function SearchResult({ country }: SearchResultPageProps) {
     return (
       <Layout>
         <SearchBar linkTo="/search/ssg/" />
-        <>
-          <p>{`CCA3 Code: Loading...`}</p>
-          <p>{`Official Name: Loading...`}</p>
-          <p>{`Common Name: Loading...`}</p>
-          <p>{`Capital: Loading...`}</p>
-          <p>{`Region: Loading...`}</p>
-          <p>{`Subregion: Loading...`}</p>
-        </>
+        <p>Loading...</p>
       </Layout>
     );
   }
@@ -31,14 +24,18 @@ export default function SearchResult({ country }: SearchResultPageProps) {
   return (
     <Layout>
       <SearchBar linkTo="/search/ssg/" />
-      <>
-        <p>{`CCA3 Code: ${country.cca3}`}</p>
-        <p>{`Official Name: ${country.name.official}`}</p>
-        <p>{`Common Name: ${country.name.common}`}</p>
-        <p>{`Capital: ${country.capital[0]}`}</p>
-        <p>{`Region: ${country.region}`}</p>
-        <p>{`Subregion: ${country.subregion}`}</p>
-      </>
+      {country ? (
+        <>
+          <p>{`CCA3 Code: ${country.cca3}`}</p>
+          <p>{`Official Name: ${country.name.official}`}</p>
+          <p>{`Common Name: ${country.name.common}`}</p>
+          <p>{`Capital: ${country.capital[0]}`}</p>
+          <p>{`Region: ${country.region}`}</p>
+          <p>{`Subregion: ${country.subregion}`}</p>
+        </>
+      ) : (
+        <p>Not found!</p>
+      )}
     </Layout>
   );
 }
@@ -53,5 +50,5 @@ export const getStaticPaths: GetStaticPaths = async () => {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const country = await getCountry(params?.cca3 as string);
 
-  return country ? { props: { country } } : { notFound: true };
+  return { props: { country } };
 };
